5d1ec1a249878c16086e0832f7c798487347dce1
крысавдоме <krisanevdome@gmail.com>
2026-02-22 00:54:08 +0300

    serve through wish

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"log"
+	"context"
+	"errors"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+
 	anim "github.com/krisavdome/diskographia/ascii"
 )
 
+const (
+	host = "0.0.0.0"
+	port = "23234"
+)
+
 type Model struct {
-	Anim   anim.Model
-	width  int
-	height int
+	Anim              anim.Model
+	width             int
+	height            int
+	hasDarkBackground bool
 }
 
 func (m Model) Init() tea.Cmd {
@@ -37,8 +56,50 @@ func (m Model) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(Model{Anim: anim.NewWithDefaults()}, tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			activeterm.Middleware(),
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("could not start server", "error", err)
 	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	log.Info("starting ssh server", "host", host, "port", port)
+	go func() {
+		if err := s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("stopping ssh server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("could not stop server", "error", err)
+	}
+}
+
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	pty, _, _ := s.Pty()
+
+	renderer := bubbletea.MakeRenderer(s)
+
+	m := Model{
+		Anim:              anim.NewWithDefaults(),
+		width:             pty.Window.Width,
+		height:            pty.Window.Height,
+		hasDarkBackground: renderer.HasDarkBackground(),
+	}
+
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
 }
